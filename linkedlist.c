5b399500ec2af026b9a327c5395694f5086090a8
SaravananB <saisaravanan12345@gmail.com>
2021-06-05 09:41:13 +0530

    Linkedlist program:             1. Creating the first node.             2. Creating the linkedlist with set of value.             3. Displaying the linkedlist value.             4. Finding the maximum value in linkedlist.             5. Searching the element in linkedlist using move to head method.             6. Inserting element in linkedlist.

---
+++ linkedlist.c
@@ -0,0 +1,116 @@
+#include <iostream>
+/**Creating the linkedlist**/
+struct Node {
+    int data;
+    struct Node *next;
+};
+struct Node *first = NULL;
+
+/**Adding value in it**/
+void createlinkedlist(int a[], int n)
+{
+    struct Node *tmp,*last;
+    first= (struct Node *)malloc(sizeof(struct Node));
+    first->data = a[0];
+    first->next = NULL;
+    last=first;
+
+    for(int i=1; i<n; i++)
+    {
+        tmp=(struct Node*)malloc(sizeof(struct Node));
+        tmp->data = a[i];
+        tmp->next = NULL;
+        last->next=tmp;
+        last=tmp;
+    }
+}
+
+/**Displaying value in linkedlist**/
+int Displaylinkedlist(struct Node *D)
+{
+    if(D!=NULL) {
+        printf("Linked list value %d\n", D->data);
+        return Displaylinkedlist(D->next);
+    }
+    else
+        return 0;
+}
+
+/**Counting the linked list value**/
+int linkedlistcount(struct Node *K)
+{
+    if(K!=NULL)
+        return 1+ linkedlistcount(K->next);
+    else
+        return 0;
+
+}
+
+/**Finding the MAX value in linkedlist**/
+int findMAX(struct Node *M)
+{
+    int ismax = -1;
+    while(M!=NULL)
+    {
+        if(ismax < M->data)
+        {
+            ismax = M->data;
+        }
+        M=M->next;
+    }
+    return ismax;
+}
+struct Node *searching(struct Node *p,  int key)
+{
+    struct Node *q=NULL;
+    while(p!=NULL)
+    {
+        if(key==p->data)
+        {
+            q->next=p->next;
+            p->next=first;
+            first=p;
+            return p;
+        }
+        q=p;
+        p=p->next;
+    }
+    return NULL;
+};
+
+void inserting(struct Node *p, int value, int pos)
+{
+    int i=1;
+    struct Node *tmp, *q;
+    tmp=(struct Node*)malloc(sizeof(struct Node));
+    tmp->data=value;
+    tmp->next=NULL;
+
+    while(pos==i)
+    {
+        p=p->next;
+        i++;
+    }
+    if(p!=NULL)
+    {
+        q->next=p->next;
+        p->next=tmp;
+        tmp->next=q->next;
+
+    }
+}
+
+int main()
+{
+    int a[]= {1,2,3,18,5,6};
+    createlinkedlist(a, 6);
+
+    printf("Linked list count value %d\n",linkedlistcount(first));
+
+    printf("Maximum Value in linked list is %d\n", findMAX(first));
+    searching(first, 5);
+    inserting(first, 3, 25 );
+    Displaylinkedlist(first);
+
+    return 0;
+}
